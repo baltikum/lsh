047d413cdb81ce8ad8b070dc04e4da061d1a7178
baltikum <mwdavidsson@outlook.com>
2021-09-13 18:54:35 +0200

    path extractor

---
+++ paths.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // getenv()
+
+
+
+int main() {
+
+	const char* paths = getenv("PATH"); // Hämtar miljövariabeln
+	//printf("PATH: %s\n",paths);
+
+	long pathlength = strlen(paths); // Kolla längden av denna
+	//printf("PATHLENGTH: %ld\n",pathlength);
+
+	char copiedpath[pathlength];
+	strcpy(copiedpath,paths); // Kopierar den då den är immutable
+	//printf("PATH COPIED: %s\n",copiedpath);
+
+
+
+	int numberofpaths = 1;
+	int pathsize = 0;
+	int largestpathsize = 0;
+	for ( int i = 0; i<pathlength ; i++) {
+		if( copiedpath[i] == ':') {
+			numberofpaths++;
+			pathsize = 0;
+		} else {
+			pathsize++;
+		}
+
+		if ( pathsize > largestpathsize) {
+			largestpathsize = pathsize;
+		}
+	}
+
+
+	char savedpaths[numberofpaths][largestpathsize+1];
+	char* pointer; // Pekare för att stega igenom copiedpath
+
+	pointer = strtok(copiedpath,":"); // Stegar fram till :
+
+	int i = 0;
+
+	while( pointer != NULL ) {
+
+		char *savepath = pointer;
+		int j = 0;
+		while(*savepath != '\0') {
+    		//printf("%c", *savepath);
+			savedpaths[i][j] = *savepath;
+    		savepath++;
+			j++;
+  		} 
+		savedpaths[i][j] = '\0';
+		printf("SAVED PATH: %s\n",savedpaths[i]);
+		++i;
+		pointer = strtok(NULL,":"); // Stega framåt till :
+	}
+}
